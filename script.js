0ef31cdb5f4505f7259c4b8640a5577abced934b
danipxx <96748325+danipxx@users.noreply.github.com>
2025-03-31 09:25:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = prompt("Qual é o seu nome?");
+alert("Olá, " + nome + "!");
+
+let numero = parseFloat(prompt("Digite um número:"));
+alert("Seu número é, " + numero + "!");
+
+var idade = parseInt(prompt("Quantos anos você tem?"));
+var ano = 2025 - idade;
+alert("você nasceu por volta de " + ano);
